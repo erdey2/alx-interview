5c0868e18e9e3de1fe33da14c1d3696b498c7609
erdey <erdeysyoum@gmail.com>
2024-07-11 04:33:28 +0000

    a function to find the minimum operations needed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Calculate a minimum operations needed"""
+
+
+def minOperations(n: int) -> int:
+    """ Find the minimum Operations needed """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
