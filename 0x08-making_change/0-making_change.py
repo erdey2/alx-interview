9adb19443f0f3b9b735df20f17fb7db685231f5a
erdey <erdeysyoum@gmail.com>
2024-08-22 13:30:44 +0000

    coin change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Making changes module """
+
+
+def makeChange(coins, total):
+    """ Generate changes needed to reach total """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
